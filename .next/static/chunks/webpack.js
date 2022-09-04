20082844a3dfd2b387b550aec6520c7fbff1eb87
SteelTitansax <manuel.portero.leiva@gmail.com>
2022-09-04 19:21:58 +0200

    New url Powerapps Management App

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "05c6c76381f7eea55a76"; }
+/******/ 		__webpack_require__.h = function() { return "453851273c63ce44d960"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
